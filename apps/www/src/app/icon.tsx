5de52bbb325b30a276d8076a2186560122b14ac4
Lu <lucien.loua@gmail.com>
2025-11-22 18:46:57 +0000

    Refactor Icon component to utilize Icons library for rendering, replacing SVG with walletKit icon for improved visual consistency and maintainability.

--- apps/www/src/app/icon.tsx
+++ apps/www/src/app/icon.tsx
@@ -1,4 +1,5 @@
 import { ImageResponse } from "next/og";
+import { Icons } from "@/lib/icons";
 
 export const size = {
   width: 32,
@@ -10,34 +11,14 @@ export default function Icon() {
   return new ImageResponse(
     <div
       style={{
-        background: "black",
         width: "100%",
         height: "100%",
         display: "flex",
         alignItems: "center",
         justifyContent: "center",
-        borderRadius: "5px",
       }}
     >
-      <svg
-        aria-hidden="true"
-        fill="none"
-        height="24"
-        stroke="white"
-        stroke-linecap="round"
-        stroke-linejoin="round"
-        stroke-width="2"
-        viewBox="0 0 24 24"
-        width="24"
-        xmlns="http://www.w3.org/2000/svg"
-      >
-        <path d="M2 10v3" fill="white" />
-        <path d="M6 6v11" fill="white" />
-        <path d="M10 3v18" fill="white" />
-        <path d="M14 8v7" fill="white" />
-        <path d="M18 5v13" fill="white" />
-        <path d="M22 10v3" fill="white" />
-      </svg>
+      <Icons.walletKit height={35} width={35} />
     </div>,
     { ...size }
   );
